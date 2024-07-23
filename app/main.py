e11f477a5b83c83538d349cbb05d791a511667b5
Andrii <andrii.khamaza0@gmail.com>
2024-07-23 23:24:38 +0300

    Solution for py-vector-class

--- app/main.py
+++ app/main.py
@@ -4,20 +4,23 @@ from typing import Union
 
 
 class Vector:
-    def __init__(self, x: Union[int, float], y: Union[int, float]) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self,
+                 coord_x: Union[int, float],
+                 coord_y: Union[int, float]
+                 ) -> None:
+        self.x = round(coord_x, 2)
+        self.y = round(coord_y, 2)
 
     def __add__(self, other: Vector) -> Vector:
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            self.x + other.x,
+            self.y + other.y
         )
 
     def __sub__(self, other: Vector) -> Vector:
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            self.x - other.x,
+            self.y - other.y
         )
 
     def __mul__(self,
@@ -27,8 +30,8 @@ class Vector:
             return self.x * other.x + self.y * other.y
 
         return Vector(
-            x=round(self.x * other, 2),
-            y=round(self.y * other, 2)
+            round(self.x * other, 2),
+            round(self.y * other, 2)
         )
 
     @classmethod
@@ -37,8 +40,8 @@ class Vector:
                                     end_point: tuple
                                     ) -> Vector:
         return cls(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
         )
 
     def get_length(self) -> Union[int, float]:
@@ -47,8 +50,8 @@ class Vector:
     def get_normalized(self) -> Vector:
         vector_len = self.get_length()
         return Vector(
-            x=self.x / vector_len,
-            y=self.y / vector_len
+            self.x / vector_len,
+            self.y / vector_len
         )
 
     def angle_between(self, other: Vector) -> float:
@@ -64,6 +67,6 @@ class Vector:
         cos_rad_angle = math.cos(math.radians(degrees))
         sin_rad_angle = math.sin(math.radians(degrees))
         return Vector(
-            x=self.x * cos_rad_angle - self.y * sin_rad_angle,
-            y=self.x * sin_rad_angle + self.y * cos_rad_angle
+            self.x * cos_rad_angle - self.y * sin_rad_angle,
+            self.x * sin_rad_angle + self.y * cos_rad_angle
         )
